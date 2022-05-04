c7948e9ec153b743511c63bc780350ac74f8665d
flizzyflo <vip3r1@gmx.de>
2022-05-04 21:38:44 +0200

    initial commit

---
+++ LinkedList.java
@@ -0,0 +1,37 @@
+
+class LinkedList
+
+{
+    int node;
+    LinkedList next;
+        
+    LinkedList(int node, LinkedList next)
+    {
+        this.node = node;
+        this.next = next;
+    }
+
+    public static void addListItem ()
+    //add item to linked list, keep order of the list and put item to the right place.
+    {
+        return; 
+    }
+
+    public static boolean searchListItem(int number)
+    //go through list to find number, return true if list contains number, else false
+    {
+        return true;
+    }
+
+
+    public static void removeListItem()
+    //should remove a list item and link the item before and the next item togehter. linkage of list should remain existing
+    {
+
+    }
+
+
+
+}
+
+
